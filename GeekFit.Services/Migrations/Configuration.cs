7f57ef5d6a71fd30b04ded1964cbb2571ce3ce23
Ryan Peters <ryan@binarydad.com>
2021-03-04 22:05:37 -0500

    working!

--- GeekFit.Services/Migrations/Configuration.cs
+++ GeekFit.Services/Migrations/Configuration.cs
@@ -1,15 +1,15 @@
 ﻿namespace GeekFit.Services.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<GeekFit.Services.GeekFitContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+
+            SetSqlGenerator("MySql.Data.MySqlClient", new MySql.Data.Entity.MySqlMigrationSqlGenerator());
         }
 
         protected override void Seed(GeekFit.Services.GeekFitContext context)
